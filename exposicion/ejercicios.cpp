d6526ab7ee02b7b30566b84abb07fa3b2b69f136
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2020-01-09 16:41:07 -0500

    09 de enero

---
+++ exposicion/ejercicios.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    int compiladores = 3;
+
+    switch(compiladores){
+        case 1: break;
+        case 2: break;
+        case 3 : cout<<"Hola";
+            break;
+
+
+    }
+
+}
